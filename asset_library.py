6189dba7520c16b272b38732245d8562a1f2ebbc
Taylor Raiford <37196043+tcraiford@users.noreply.github.com>
2026-07-07 12:34:38 -0400

    260707 Mid-day
    -added lists for category selection with subcategory selection
        -clears out sub lists if changing upper list selection
    -added automatic list population when a new asset library directory is build via admin
    
    TODO:
    - need a button that triggers category list population method in case directory is changed
    - add final list to display contents of sub_subcategory list (the folders for each asset in the library)
    - button to create new asset folder w/ an archive folder and dummy thumbnail inside
    - thumbnail preview

--- asset_library.py
+++ asset_library.py
@@ -3,7 +3,8 @@ import os
 import subprocess
 import sys
 
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QMainWindow, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel, QLineEdit, QMessageBox, QDialog)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QMainWindow, QWidget, QVBoxLayout, QPushButton,
+                               QFileDialog, QLabel, QLineEdit, QMessageBox, QDialog, QListWidget, QListWidgetItem)
 from PySide6.QtCore import Qt
 from pathlib import Path
 
@@ -103,6 +104,22 @@ class AssetLibraryApp(QMainWindow):
         self.HARDCODED_PASSWORD = "0000"
         self.is_admin = False
 
+        # setting up the category lists
+        self.category_list = QListWidget()
+        self.subcategory_list = QListWidget()
+        self.sub_subcategory_list = QListWidget()
+        # When a user selects a main category, run the "load_subcategories" method
+        # this is just another activation, similar to clicked for a button widget
+        self.category_list.itemSelectionChanged.connect(self.load_subcategories)
+        # when a user selects a subcategory from the subcategory list, load the sub_subcategories
+        self.subcategory_list.itemSelectionChanged.connect(self.load_sub_subcategories)
+
+        # category list layout
+        list_layout = QHBoxLayout()
+        list_layout.addWidget(self.category_list)
+        list_layout.addWidget(self.subcategory_list)
+        list_layout.addWidget(self.sub_subcategory_list)
+
         self.directory_line = QLineEdit()
         self.directory_line.setPlaceholderText("Enter the base directory path here...")
         self.directory_line.returnPressed.connect(self.set_base_directory)
@@ -129,6 +146,7 @@ class AssetLibraryApp(QMainWindow):
         # define the positioning of content on the main_layout tool
         main_layout.addWidget(description)
         main_layout.addLayout(dir_layout)
+        main_layout.addLayout(list_layout)
 
 
     def create_starting_library(self):
@@ -141,6 +159,9 @@ class AssetLibraryApp(QMainWindow):
         self.current_directory = self.directory_line.text()
         if not os.path.exists(self.current_directory):
             QMessageBox.warning(self, "Directory Not Found", "The specified directory does not exist.")
+
+        else:
+            self.populate_list(Path(self.current_directory), self.category_list)
 
     def check_admin_access(self):
         # if already entered password, then access is automatic
@@ -157,7 +178,64 @@ class AssetLibraryApp(QMainWindow):
             else:
                 QMessageBox.critical(self, "Incorrect Password", "The password entered is incorrect.")
         return False
-                
+
+    """this method clears the list_widget from any leftovers. It then looks at the directory and all its contents and says it only cares if the contents item is a folder.
+    It then takes each folder and removes the file directory from it so it is only the name of the folder but then stores that removed directory bit into slot 100 for that specific folder.
+    This way, we only see the folder's name but that folder still has its directory stored to it."""            
+    def populate_list(self, folder_path, list_widget):
+        # clear out any old text each time this is called
+        list_widget.clear()
+
+        # looks at all items in this directory
+        for path in sorted(folder_path.iterdir()):
+            # .is_dir only passes true if the item inside this folder is a folder
+            if path.is_dir():
+                # remove the directory and only keep the name of the found item
+                item = QListWidgetItem(path.name)
+
+                # put the full directory and name, combined, into slot 100
+                # this isn't visible to the user but the code reads it when clicked
+                # slot 100 is not a global slot, it is tied to whatever "item" we are on so itemA has a slot 100 and itemB has a slot 100 etc etc etc
+                item.setData(100, path)
+
+                list_widget.addItem(item)
+
+    """These are the methods that are called when a folder in the lists is selected that tells the next list what to display. They are called by the itemSelectionChanged action inside __innit__"""
+    def load_subcategories(self):
+        # wipe downstream columns clean so if you change your category selection, it doesn't get confused by your subcategory selection data
+        self.subcategory_list.clear()
+        self.sub_subcategory_list.clear()
+
+        # get whatever item is currently selected
+        selected_items = self.category_list.selectedItems()
+
+        if selected_items:
+            # if multiple lines are selected, it only returns the first
+            chosen_item = selected_items[0]
+
+            # look inside data slot 100 for the selected item and get the directory path
+            next_folder_path = chosen_item.data(100)
+
+            # runs the populate_list method and feeds it the file path for the selected folder we just established and also tells the populate_list method we want to send that data to the subcategory_list
+            self.populate_list(next_folder_path, self.subcategory_list)
+
+    """This follows the same logic as load_subcategories but it triggers when an item in column 2 is clicked, clears out only column 3, and populates column 3. """
+    def load_sub_subcategories(self):
+        # wipe only the final column clean. We don't want to wipe the column above us
+        self.sub_subcategory_list.clear()
+
+        # determine which row was highlighted in the subcategory list
+        selected_items = self.subcategory_list.selectedItems()
+
+        if selected_items:
+            # if something is selected, we tell the code to only look at the first item selected
+            chosen_item = selected_items[0]
+
+            # look inside data slot 100 for the selected item and get the directory path
+            next_folder_path = chosen_item.data(100)
+
+            # runs the populate_list method and feeds it the file path for the selection we just established and also tells the populate list method we want to send that data to the sub_subcategory_list)
+            self.populate_list(next_folder_path, self.sub_subcategory_list)
 
     def open_directory(self):
         directory = self.directory_line.text()
